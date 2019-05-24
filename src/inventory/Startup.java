4f91aca4ea1ce16d46dece582d7065370168b67f
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2019-05-24 17:53:06 +0200

    Uncomment Borrower functions

--- src/inventory/Startup.java
+++ src/inventory/Startup.java
@@ -1,6 +1,11 @@
 package inventory;
 
-public class Startup /*implements Borrower*/ {
+import user.Borrower;
+
+import java.util.Date;
+import java.util.List;
+
+public class Startup implements Borrower {
     private final String id;
     private String name;
     private final Incubator incubator;
@@ -25,5 +30,21 @@ public class Startup /*implements Borrower*/ {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void borrow(Equipment equipment, Date borrowDate, Date returnDate, String reason) {
+        //TODO
+    }
+
+    @Override
+    public List<Equipment> getBorrowedEquipement() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void returnEquipement(Equipment equipment) {
+        //TODO
     }
 }
--- src/user/Borrower.java
+++ src/user/Borrower.java
@@ -1,13 +1,16 @@
 package user;
 
+import inventory.Equipment;
+import inventory.Stock;
+
 import java.util.Date;
 import java.util.List;
 
 public interface Borrower {
-    /*
-    public void borrow (Equipement equipement, Date borrowDate, Date returnDate, String reason);
-    public List<Equipement> getBorrowedEquipement();
-    public void returnEquipement(Equipement equipement);
-    public void borrow (Stock<Equipement> equipements,Date borrowDate, Date returnDate, String reason);
-    */
+
+    public void borrow (Equipment equipment, Date borrowDate, Date returnDate, String reason);
+    public List<Equipment> getBorrowedEquipement();
+    public void returnEquipement(Equipment equipment);
+    /*public void borrow (Stock<Equipment> equipments, Date borrowDate, Date returnDate, String reason);*/
+
 }
