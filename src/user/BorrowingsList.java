7461b7f3601685bf241ace33d656054fa5ee0dde
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-05-24 17:35:40 +0200

    Added Borrowing class and BorrowingsList singleton

---
+++ src/user/BorrowingsList.java
@@ -0,0 +1,33 @@
+package user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BorrowingsList {
+    //private List<Borrowable> borrowedItems;
+    private static BorrowingsList ourInstance = new BorrowingsList();
+
+    public static BorrowingsList getInstance() {
+        return ourInstance;
+    }
+
+    /*private BorrowingsList() {
+        this.borrowedItems = new ArrayList<Borrowable>();
+    }*/
+
+    //public List<Borrowable> getBorrowedItems(){return null;}
+
+    //public boolean isBorrowed(Borrowable item){return false;}
+
+    //public void addBorrowedItem(Borrowable item){}
+
+    //public void removeBorrowedItel(Borrowable item){}
+
+    public List<Borrowing> getBorrowings(){return null;}
+
+    public List<Borrowing> getBorrowings(String reason){return null;}
+
+    public List<Borrowing> getBorrowings(Borrower borrower){return null;}
+
+    public List<Borrowing> getLateBorrowings(){return null;}
+}
