048ec68fadf31b048a9ec3e3f59842dc1095550e
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-05-24 16:16:12 +0200

    Added People and Student simple implementation

---
+++ src/user/Student.java
@@ -0,0 +1,7 @@
+package user;
+
+public class Student extends People{
+    public Student(String socialSecurityNumber,String firstName, String surName, String address, String phoneNumber, String email){
+        super(socialSecurityNumber,firstName,surName,address,phoneNumber,email);
+    }
+}
