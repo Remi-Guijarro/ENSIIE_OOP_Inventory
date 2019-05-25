bd07b0c96a2471f078a9b012b816bf7dac3d3721
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2019-05-25 13:05:35 +0200

    Add database package with interfaces and class
    Needs to be implemented.

---
+++ src/database/JSONDatabase.java
@@ -0,0 +1,13 @@
+package database;
+
+public class JSONDatabase implements Savable, Loadable{
+    @Override
+    public void load() {
+        //TODO
+    }
+
+    @Override
+    public void save() {
+        //TODO
+    }
+}
