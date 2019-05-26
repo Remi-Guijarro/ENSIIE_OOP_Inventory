d5d19a6d38fca160c31482fbb58b0be23e3efef2
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-05-26 21:15:00 +0200

    Added a first ihm

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
 import database.SerializeDatabase;
 import inventory.ContextContainer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application{
+    public static Stage mainPrimaryStage;
+    public static BorderlessScene mainScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mainPrimaryStage = primaryStage;
         Parent root = FXMLLoader.load(this.getClass().getClassLoader().getResource("gui/main.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Inventory");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        BorderlessScene bordelessScene = new BorderlessScene(primaryStage, StageStyle.UNDECORATED, root, 1000, 800);
+        mainScene = bordelessScene;
+        primaryStage.setScene(bordelessScene);
+        bordelessScene.setMoveControl(root.lookup("#header"));
+        bordelessScene.setSnapEnabled(true);
+        bordelessScene.removeDefaultCSS();
         SerializeDatabase ser = new SerializeDatabase();
         ser.save(new ContextContainer());
         primaryStage.show();
--- src/MainController.java
+++ src/MainController.java
@@ -1,3 +1,12 @@
+import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+import java.io.IOException;
+
 public class MainController {
-    // TODO
 }
