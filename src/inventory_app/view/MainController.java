a03fd558d38b312d9df4072a3687ff1d38b300a4
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-06-14 15:20:21 +0200

    Feature -> display the users list on a new tab ; create a new tab onto the TabPane for user display

--- src/inventory_app/view/MainController.java
+++ src/inventory_app/view/MainController.java
@@ -2,13 +2,19 @@ package inventory_app.view;
 
 import inventory_app.Main;
 import inventory_app.model.inventory.Borrower;
+import inventory_app.model.users.Student;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 
 public class MainController {
+
+    @FXML
+    private TabPane tabs;
+
 
     public void exit(){
         Main.mainPrimaryStage.close();
@@ -24,5 +30,22 @@ public class MainController {
         }else{
             Main.mainPrimaryStage.setMaximized(true);
         }
+    }
+
+    public void openUserView() {
+      /*  Tab userTab = new Tab();
+        AnchorPane userTabRootNode = new AnchorPane();
+        userTab.setContent(userTabRootNode);
+        TableView userTableView = new TableView();
+        TableColumn name = new TableColumn();
+        name.setText("Name");
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        userTableView.getColumns().add(name);
+        userTableView.prefWidthProperty().bind(userTabRootNode.prefWidthProperty());
+        userTableView.prefHeightProperty().bind(userTabRootNode.prefHeightProperty());
+        Main.contextContainer.getUsers().get().forEach(item -> userTableView.getItems().add(item));
+        userTabRootNode.getChildren().add(userTableView);
+        tabs.getTabs().add(userTab);
+        */
     }
 }
